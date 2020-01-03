b13166f65b695489657f44c8f10db5fb2f78d60d
Ryan Kramer <default.kramer@gmail.com>
2020-01-03 12:34:48 -0600

    fix doc coverage test

--- morsel-doc/doc-coverage.rkt
+++ morsel-doc/doc-coverage.rkt
@@ -2,14 +2,8 @@
 (module+ test
   (require doc-coverage
            morsel-lib
-           morsel-lib/sql)
+           morsel-lib/sql
+           morsel-lib/sql/dialect)
   (check-all-documented 'morsel-lib)
   (check-all-documented 'morsel-lib/sql)
-
-  ; TODO why does this crash?
-  ;(check-all-documented 'morsel-lib/sql/dialect)
-  ; The root cause is that this crashes:
-  ;(module->exports 'morsel-lib/sql/dialect)
-  ; Leave a failing test here to remind me
-  (check-documented 'morsel-lib/sql/dialect 'TODO)
-  )
+  (check-all-documented 'morsel-lib/sql/dialect))
